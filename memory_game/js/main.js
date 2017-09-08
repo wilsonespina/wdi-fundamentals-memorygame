2f47187556579f3b60d63b8a4887357604a55afa
Wilson Espina <Wilson@Wilsons-MacBook-Air.local>
2017-09-08 21:52:26 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,22 @@
  console.log("Up and running!");
 
- var cardOne = "queen";
- var cardTwo = "queen";
- var cardThree = "king";
- var cardFour = "king";
+var cards = ["queen","queen","king","king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+var cardsInPlay = [];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+}	else {
+		alert("Sorry try again");
+}
+
